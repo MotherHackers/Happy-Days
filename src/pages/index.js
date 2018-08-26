42f5e0cd64ff4016d7eaff84ccca3f08feb97e53
RodrigoCasale <rodrigo_casale22@hotmail.com>
2018-08-25 20:09:14 -0500

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hola mundo</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
