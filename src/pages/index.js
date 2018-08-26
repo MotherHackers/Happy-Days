7890eef4e16e68cf035c93b8c0b8580df7078990
RodrigoCasale <rodrigo_casale22@hotmail.com>
2018-08-25 21:36:46 -0500

    g

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,22 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
 const IndexPage = () => (
   <div className="jumbotron">
-    <h1 className="">¿Como te sientes hoy?</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+    <div className="container">
+      <h1 className="display-4 jumboTitle">¿Como te sientes hoy?</h1>
+      <br></br>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+
+      
+        <br></br>
+        <a className="btn btn-primary btn-lg" href="/page2" role="button">Preocupado</a>
+        <a className="btn btn-primary btn-lg" href="/page2" role="button">Animado</a>
+        <a className="btn btn-primary btn-lg" href="/page2" role="button">Chistoso</a>
+      </div>
+  </div>    
+
 )
 
 export default IndexPage
